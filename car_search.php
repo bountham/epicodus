eab7fe016876c7453a0e330a0b6c71eadba31b99
Cynthia Wilhelm & tommy bountham <student@epicodus.com>
2015-03-02 12:23:45 -0800

    Add rudimentary search page file

---
+++ car_search.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Car Search Form!</title>
+        <link rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Car Search</h1>
+            <p>Enter search parameters</p>
+
+            <div>
+                <form action="car_index.php">
+                    <div class="form-group">
+                        <label for="price">Maximum price:</label>
+                        <input id="price" name="price" class="form-control"
+                        type="number">
+                    </div>
+                    <button type="submit" class="btn-success">Let's rock</button>             
+                </form>
+            </div>
+        </div>
+
+
+    </body>
+</html>
