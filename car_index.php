a7cb83265205b104f7f54fbe83693b64e43a0af2
Cynthia Wilhelm & tommy bountham <student@epicodus.com>
2015-03-02 11:21:21 -0800

    Create get and set functions in index file

--- car_index.php
+++ car_index.php
@@ -12,14 +12,46 @@ class Car {
         $this->image_path = $image_path;
     }
 
-    $porsche = new Car("2014 Porsche 911", 114991, 7864, "img/porsche_911.jpg");
-    $ford = new Car("2011 Ford F450", 55995, 14241, "img/ford_truck.jpg");
-    $lexus = new Car("2013 Lexus RX 350", 44700, 20000, "img/lexus_2013.jpg");
-    $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "img/mercedes_cls550.jpg");
+    function getMakeModel() {
+        return $this->make_model;
+    }
 
-    $cars = array($porsche, $ford, $lexus, $mercedes);
+    function getPrice() {
+        return $this->price;
+    }
+
+    function getMileage() {
+        return $this->mileage;
+    }
+
+    function getImagePath() {
+        return $this->image_path;
+    }
+
+    function setMakeModel($newMakeModel){
+        $this->make_model = $newMakeModel;
+    }
+
+    function setPrice($newPrice) {
+        $this->price = $newPrice;
+    }
+
+    function setMileage ($newMileage){
+        $this->mileage = $newMileage;
+    }
+
+    function setImagePath ($newImagePath){
+        $this->image_path = $newImagePath;
+    }
 
 }
+
+$porsche = new Car("2014 Porsche 911", 114991, 7864, "img/porsche_911.jpg");
+$ford = new Car("2011 Ford F450", 55995, 14241, "img/ford_truck.jpg");
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000, "img/lexus_2013.jpg");
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "img/mercedes_cls550.jpg");
+
+$cars = array($porsche, $ford, $lexus, $mercedes);
 
 ?>
 
@@ -32,6 +64,15 @@ class Car {
             <link rel="stylesheet" href="css/car_style.css">
     </head>
     <body>
+        <div class="container">
+          <!--test-->
+            <?php
+                foreach($cars as $car) {
+                    $tag = $car->getMakeModel() . "\n";
+                    echo $tag;
+                }
 
+             ?>
+        </div>
     </body>
 </html>
